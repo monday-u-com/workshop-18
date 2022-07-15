c6e9afaa7e25813a1ee91ad68ab5f83b034c2973
liortlv <99750449+liortlv@users.noreply.github.com>
2022-07-15 12:07:36 +0300

    Add change test

--- src/cube.js
+++ src/cube.js
@@ -1,21 +1,21 @@
 class Cube {
-  constructor(length) {
-    this.length = length;
-  }
+   constructor(length) {
+      this.length = length;
+   }
+   //Change
+   getSideLength() {
+      return this.length;
+   }
 
-  getSideLength () {
-    return this.length;
-  }
+   getSurfaceArea() {
+      return this.length * this.length * 6;
+   }
 
-  getSurfaceArea () {
-    return (this.length * this.length) * 6;
-  }
-
-  getVolume () {
-    return Math.pow(this.length,3);
-  }
+   getVolume() {
+      return Math.pow(this.length, 3);
+   }
 }
 
 module.exports = {
-  Cube:Cube
-}
+   Cube: Cube,
+};
